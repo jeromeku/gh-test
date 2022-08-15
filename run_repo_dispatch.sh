58cf0fd0cd8e51e6e917155a2a333144018a64ae
jeromeku <jerome.ku@gmail.com>
2022-08-15 14:27:42 -0700

    add dispatch function

---
+++ run_repo_dispatch.sh
@@ -0,0 +1,6 @@
+curl -X POST \
+-H "Accept: application/vnd.github.v3+json" \
+-H "Authorization: token ${GITHUB_BITGO_PAT}" \
+https://api.github.com/repos/jeromeku/gh-test/dispatches \
+-d '{"event_type":"my_dispatch", "client_payload": { "type": "my_dispatch", "name": "Jerome" }}'
+
