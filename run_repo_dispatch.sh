d109498bdc1b1919904ea4f4cb4733533804df59
jeromeku <jerome.ku@gmail.com>
2022-08-15 14:31:28 -0700

    update if condition

--- run_repo_dispatch.sh
+++ run_repo_dispatch.sh
@@ -2,5 +2,5 @@ curl -X POST \
 -H "Accept: application/vnd.github.v3+json" \
 -H "Authorization: token ${GITHUB_BITGO_PAT}" \
 https://api.github.com/repos/jeromeku/gh-test/dispatches \
--d '{"event_type":"my_dispatch", "client_payload": { "type": "my_dispatch", "name": "Jerome" }}'
+-d '{"event_type":"my_dispatch", "client_payload": { "type": "function", "message": "Hello from function", "name": "Jerome" }}'
 
